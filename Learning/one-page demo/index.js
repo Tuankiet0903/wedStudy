599b659f0c3262bbb199c9e835dc625ee58e0462
TuanKiet0903 <htk09032003@gmail.com>
2023-04-13 15:01:15 +0700

    update 13/4

--- Learning/one-page demo/index.js
+++ Learning/one-page demo/index.js
@@ -22,3 +22,29 @@ modal.addEventListener('click', hideBuyTicket)
 modalContainer.addEventListener('click',function(event){
     event.stopPropagation()
 })
+
+
+// responsive mobile
+//open/close menu
+const header = document.getElementById('header');
+const mobileMenu = document.getElementById('mobile-menu');
+const headerHeight = header.clientHeight; 
+
+
+mobileMenu.onclick = function(){
+    let isClosed = header.clientHeight === headerHeight;
+    if(isClosed){
+        header.style.height = 'auto';
+    }else{
+        header.style.height = null;
+    }
+}
+
+//auto close when choose
+const menuItems = document.querySelectorAll('#nav li a[href*="#"');
+for(let i = 0; i < menuItems.length; i++){
+    let menuItem = menuItems[i];
+    menuItem.onclick = function(){
+        header.style.height = null;
+    }
+}
